7be7f35eb965bf481fa4e66824f463bf040cd2c6
Nolan Geiger <ngeiger.laba@solvd.com>
2023-06-11 16:25:58 -0400

    Update student.xsd with revised structure
    Modified the student.xsd file to reflect the updated structure of the student.xml file. Added elements for admission_year, department_id, and department_name. Updated the xs:complexType and xs:sequence accordingly.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,63 +5,56 @@ import org.example.model.Course;
 import org.example.service.StudentService;
 import org.example.service.CourseService;
 import org.example.util.ConnectionPool;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            // Get connection from the connection pool
-            Connection connection = ConnectionPool.getConnection();
+            // Parse the XML file and retrieve the student data
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File("C:\\Users\\Nolan\\IdeaProjects\\University_Course_Management\\src\\main\\resources\\student.xml"));
 
-            // New instances of StudentService and CourseService
-            StudentService studentService = new StudentService();
-            CourseService courseService = new CourseService();
+            Element root = document.getDocumentElement();
+            NodeList studentList = root.getElementsByTagName("student");
 
-            // Create new student
-            Student student = new Student("Tony Soprano", 2022, 2, "Philosophy");
-            studentService.create(student);
+            // Establish a connection to the MySQL database
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/univcoursemgmt", "root", "password");
 
-            // Get student by ID
-            Student retrievedStudent = studentService.getById(5);
-            System.out.println("Retrieved student: " + retrievedStudent);
+            // Iterate over the student data and insert/update records in the database
+            for (int i = 0; i < studentList.getLength(); i++) {
+                Element studentElement = (Element) studentList.item(i);
+                String studentName = studentElement.getAttribute("student_name");
+                int admissionYear = Integer.parseInt(studentElement.getAttribute("admission_year"));
+                int departmentId = Integer.parseInt(studentElement.getAttribute("department_id"));
+                String departmentName = studentElement.getAttribute("department_name");
 
-            // Update student
-            retrievedStudent.setStudentName("Henry Hill");
-            studentService.update(retrievedStudent);
+                // Perform database operations (insert/update) based on your requirements
+                // Use PreparedStatement to prevent SQL injection
+                PreparedStatement statement = connection.prepareStatement("INSERT INTO student (student_name, admission_year, department_id, department_name) VALUES (?, ?, ?, ?)");
+                statement.setString(1, studentName);
+                statement.setInt(2, admissionYear);
+                statement.setInt(3, departmentId);
+                statement.setString(4, departmentName);
+                statement.executeUpdate();
+                statement.close();
+            }
 
-            // Delete student
-            studentService.delete(3);
-
-            // Get all students
-            List<Student> allStudents = studentService.getAll();
-            System.out.println("All students: " + allStudents);
-
-            // Create new course
-            Course course = new Course("Introduction to Programming", 3, 1, 1, 1, 1, "An introductory course on programming");
-            courseService.create(course);
-
-            // Get course by ID
-            Course retrievedCourse = courseService.getById(1);
-            System.out.println("Retrieved course: " + retrievedCourse);
-
-            // Update course
-            retrievedCourse.setCourseName("Advanced Programming");
-            courseService.update(retrievedCourse);
-
-            // Delete course
-            courseService.delete(2);
-
-            // Get all courses
-            List<Course> allCourses = courseService.getAll();
-            System.out.println("All courses: " + allCourses);
-
-            // Close connection
+            // Close the database connection
             connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
