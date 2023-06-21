3ada3fa678ded39da1935414300e317d8715b3cf
Nolan Geiger <ngeiger.laba@solvd.com>
2023-06-21 10:12:11 -0400

    Added dependencies to implement JAXB. Implemented the Grades.java model. Marshalled and unmarshalled Grades.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,48 @@
 package org.example;
 
 import org.example.model.Department;
+import org.example.model.jaxb.Grades;
 import org.example.service.DepartmentService;
 import org.example.util.ConnectionPool;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.sql.Connection;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+//        try {
+//            Grades grade = new Grades(20, 10, "97", 1, 1);
+//
+//            File file = new File("src/main/resources/jaxbFiles/grade.xml");
+//            JAXBContext jaxbContext = JAXBContext.newInstance(Grades.class);
+//            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+//            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+//            jaxbMarshaller.marshal(grade, file);
+//            jaxbMarshaller.marshal(grade, System.out);
+//
+//        } catch (JAXBException e) {
+//            e.printStackTrace();
+//        }
+
         try {
-            // Instantiate the DepartmentService
-            DepartmentService departmentService = new DepartmentService();
+            File file = new File("src/main/resources/jaxbFiles/grade.xml");
+            JAXBContext jaxbContext = JAXBContext.newInstance(Grades.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Grades grade = (Grades) jaxbUnmarshaller.unmarshal(file);
 
-            // Create a new department
-            Department department1 = new Department("Computer Science");
-            departmentService.create(department1);
-            System.out.println("Created department: " + department1);
+            //Accessing the parsed object
+            System.out.println("Grade ID: " + grade.getGradeId());
+            System.out.println("Enrollment ID: " + grade.getEnrollmentId());
+            System.out.println("Grade: " + grade.getGrade());
+            System.out.println("Course ID: " + grade.getCourseId());
+            System.out.println("Student ID: " + grade.getStudentId());
 
-            // Get department by ID
-            int departmentId = department1.getDepartmentId();
-            Department retrievedDepartment1 = departmentService.getById(departmentId);
-            System.out.println("Retrieved department: " + retrievedDepartment1);
-
-            // Update department
-            retrievedDepartment1.setDepartmentName("Information Technology");
-            departmentService.update(retrievedDepartment1);
-            System.out.println("Updated department: " + retrievedDepartment1);
-
-            // Delete department
-            departmentService.delete(departmentId);
-            System.out.println("Department deleted.");
-
-            // Get all departments
-            List<Department> allDepartments = departmentService.getAll();
-            System.out.println("All departments:");
-            for (Department dept : allDepartments) {
-                System.out.println(dept);
-            }
-        } catch (Exception e) {
+        } catch (JAXBException e){
             e.printStackTrace();
         }
     }
---
+++ src/main/java/org/example/model/jaxb/Grades.java
@@ -0,0 +1,79 @@
+package org.example.model.jaxb;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "grade")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Grades {
+
+    @XmlElement
+    private int gradeId;
+
+    @XmlElement
+    private int enrollmentId;
+
+    @XmlElement
+    private String grade;
+
+    @XmlElement
+    private int courseId;
+
+    @XmlElement
+    private int studentId;
+
+    // Constructors
+    public Grades() {
+    }
+
+    public Grades(int gradeId, int enrollmentId, String grade, int courseId, int studentId) {
+        this.gradeId = gradeId;
+        this.enrollmentId = enrollmentId;
+        this.grade = grade;
+        this.courseId = courseId;
+        this.studentId = studentId;
+    }
+
+    // Getters and Setters
+    public int getGradeId() {
+        return gradeId;
+    }
+
+    public void setGradeId(int gradeId) {
+        this.gradeId = gradeId;
+    }
+
+    public int getEnrollmentId() {
+        return enrollmentId;
+    }
+
+    public void setEnrollmentId(int enrollmentId) {
+        this.enrollmentId = enrollmentId;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
